bc9406ef4fa1c6d7e33901f1ac7c584abfc1415c
Dorian Guilmain <guilmain.dorian@gmail.com>
2023-08-11 00:29:09 +0200

    ✨ Share room & user states

--- src/services/socket.js
+++ src/services/socket.js
@@ -14,15 +14,49 @@ const ioService = server => {
     socket.on('join-room', ({ roomId, userId }) => {
       socket.join(roomId);
 
-      socket.to(roomId).emit('user-connected', userId);
+      socket.to(roomId).emit('user-in-room', { roomId, userId });
+      console.log(`${userId} enter in the room`);
 
       socket.on('disconnect', () => {
-        socket.to(roomId).emit('user-disconnected', userId);
+        socket.to(roomId).emit('user-out-room', { roomId, userId });
       });
     });
 
-    socket.on('player-state', ({ roomId, userId, state }) => {
-      socket.to(roomId).emit('player-state', { userId, state });
+    socket.on('join-game', ({ roomId, userId }) => {
+      socket.join(roomId);
+
+      socket.to(roomId).emit('user-in-game', userId);
+
+      socket.on('disconnect', () => {
+        socket.to(roomId).emit('user-out-game', userId);
+      });
+    });
+
+    socket.on(
+      'player-state',
+      ({ roomId, userId, isOwner, username, state, lives, ready }) => {
+        console.log(`${userId} (${username}) share is state`);
+        socket.to(roomId).emit('player-state', {
+          userId,
+          isOwner,
+          username,
+          state,
+          lives,
+          ready,
+        });
+      }
+    );
+
+    socket.on('room-state', ({ roomId, state, currentTime }) => {
+      console.log(`Room share is state (${state}) & time ${currentTime}`);
+      socket.to(roomId).emit('room-state', {
+        state,
+        currentTime,
+      });
+    });
+
+    socket.on('start-game', ({ roomId }) => {
+      socket.to(roomId).emit('start-game');
     });
 
     socket.on('play-video', ({ roomId, currentTime }) => {
